acd9cff7a4aacd25f6d7d2b490f6bbe7d65581a2
Oyama <Oyama@100.64.1.73>
2021-10-03 21:44:36 +0900

    first commit

---
+++ SpringSecLogin/src/main/java/com/tsuvi/SpringSecLogin/SpringSecLoginApplication.java
@@ -0,0 +1,13 @@
+package com.tsuvi.SpringSecLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecLoginApplication.class, args);
+	}
+
+}
---
+++ SpringSecLogin/src/test/java/com/tsuvi/SpringSecLogin/SpringSecLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsuvi.SpringSecLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
